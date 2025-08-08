75996d4ee854b7d9d03d8ed612e98035dc1c4225
alfaover27 <over27.a@gmail.com>
2025-08-08 06:36:57 -0400

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -24,6 +24,7 @@ export interface KirimData {
   tolandi_prechisleniya: number
   tolandi_karta: number
   qoldiq: number
+  qoldiq_avans: number // NEW FIELD
   last_updated: string
   created_at: string
 }
